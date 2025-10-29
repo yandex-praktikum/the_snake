e1d41a7164707fa7ac13152cd6b8ef92a5c97fb0
zaro6289-gif <zaro6289@gmail.com>
2025-10-29 23:51:09 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -161,17 +161,17 @@ def handle_keys(game_object):
             pygame.quit()
             raise SystemExit
         elif event.type == pygame.KEYDOWN:
-            if (event.key == pygame.K_UP and 
-                    game_object.direction != DOWN):
+            if (event.key == pygame.K_UP
+                    and game_object.direction != DOWN):
                 game_object.next_direction = UP
-            elif (event.key == pygame.K_DOWN and 
-                    game_object.direction != UP):
+            elif (event.key == pygame.K_DOWN
+                    and game_object.direction != UP):
                 game_object.next_direction = DOWN
-            elif (event.key == pygame.K_LEFT and 
-                    game_object.direction != RIGHT):
+            elif (event.key == pygame.K_LEFT
+                    and game_object.direction != RIGHT):
                 game_object.next_direction = LEFT
-            elif (event.key == pygame.K_RIGHT and 
-                    game_object.direction != LEFT):
+            elif (event.key == pygame.K_RIGHT
+                    and game_object.direction != LEFT):
                 game_object.next_direction = RIGHT
 
 
